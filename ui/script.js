a309927910ce929394e663bb877036813ba4b46b
FAXES <31368070+FAXES@users.noreply.github.com>
2018-02-20 21:40:07 +1100

    Released

--- ui/script.js
+++ ui/script.js
@@ -3,40 +3,12 @@ $(function(){
 		window.addEventListener('message', function(event){
 
 			var item = event.data;
-			/* ----HELP SECTION---- */
-			if (item !== undefined && item.type === "help") {
+			if (item !== undefined && item.type === "logo") {
 
 				if (item.display === true) {
-					$('#help').delay(100).fadeIn( "slow" );
+					$('#logo').delay(100).fadeIn( "slow" );
 				} else if (item.display === false) {
-					$('#help').fadeOut( "slow" );
-				}
-			}
-			/* ----RULES SECTION---- */
-			if (item !== undefined && item.type === "rules") {
-
-				if (item.display === true) {
-					$('#rules').delay(100).fadeIn( "slow" );
-				} else if (item.display === false) {
-					$('#rules').fadeOut( "slow" );
-				}
-			}
-			/* ----COMMANDS SECTION---- */
-			if (item !== undefined && item.type === "cmds") {
-
-				if (item.display === true) {
-					$('#cmds').delay(100).fadeIn( "slow" );
-				} else if (item.display === false) {
-					$('#cmds').fadeOut( "slow" );
-				}
-			}
-			/* ----INFO SECTION---- */
-			if (item !== undefined && item.type === "info") {
-
-				if (item.display === true) {
-					$('#info').delay(100).fadeIn( "slow" );
-				} else if (item.display === false) {
-					$('#info').fadeOut( "slow" );
+					$('#logo').fadeOut( "slow" );
 				}
 			}
 		});
